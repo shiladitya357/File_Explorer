9cc645d6ea1324efdf679cf57e6129d6a78ad3fd
shiladitya357 <shiladitya357@gmail.com>
2020-07-05 13:37:25 +0530

    Initial Commit

---
+++ playground/path.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+console.log(path.join('Node', 'fileexplorer'));
+
+console.log(path.normalize('../../Node/../Built in Modules fs'));
+
+console.log(path.resolve('../../Node/../Built in Modules fs'));
